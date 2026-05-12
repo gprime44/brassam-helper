d6a573cc99cc6031f156b1d406f3a5302c3ddd42
guillaume.prime <guillaume.prime@nickel.eu>
2026-05-12 10:21:57 +0200

    :rocket: :wrench: chore: add build and push automation and fix docker execution.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,16 +1,11 @@
-import { defineConfig } from 'vitest/config'
-import react from '@vitejs/plugin-react'
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api': 'http://localhost:8080'
-    }
-  },
   test: {
     globals: true,
-    environment: 'jsdom',
+    environment: "jsdom",
   },
-})
+});
